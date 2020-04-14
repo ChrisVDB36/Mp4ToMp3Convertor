532a30ff34cae530a0b4e3eb858a6acec10cb328
Chris Van den Broeck <ChrisVdB36@hotmail.com>
2020-04-14 18:34:39 +0200

    File model added

---
+++ Mp4ToMp3Convertor/Models/MediaFile.cs
@@ -0,0 +1,29 @@
+﻿namespace Mp4ToMp3Convertor.Models
+{
+    public class MediaFile
+    {
+        public string FilePath { get; set; }
+        public string FileName { get; set; }
+        public string Action { get; set; }
+        public int Progress { get; set; }
+
+        // Equals override, used in listview contains check
+        public override bool Equals(object obj)
+        {
+            if (obj != null && obj is MediaFile mediaFile)
+            {
+                return FilePath.Equals(mediaFile.FilePath);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // Return hashcode while using equals
+        public override int GetHashCode()
+        {
+            return FilePath.GetHashCode();
+        }
+    }
+}
